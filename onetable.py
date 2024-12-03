b63d4745fd9080568a464fb0f9c5f4e5efe8dfae
Nate Buttke <nate-web@riseup.net>
2024-12-03 11:34:59 -0800

    scrape all links in list

--- onetable.py
+++ onetable.py
@@ -2,19 +2,32 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 import os
+import time
 
-url = 'https://www.tomshardware.com/news/gpus-historical-ebay-pricing'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+with open('monthly_links.txt', 'r') as file:
+    urls = [line.strip() for line in file if line.strip()]
 
-os.mkdir(soup.title.string)
-os.chdir(soup.title.string)
+data_root = 'data'
+os.makedirs(data_root, exist_ok=True)
+os.chdir(data_root)
 
-tables = soup.find_all('table')
+for url in urls:
+    time.sleep(0.2)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-for i, table in enumerate(tables):
-    df = pd.read_html(str(table))[0]
-    
-    csv_filename = f'table_{i}.csv'
-    df.to_csv(csv_filename, index=False)
-    print(f"Saved table {i} to {csv_filename}")
+    folder_name = soup.title.string.replace(" ", "_").replace("/", "_")
+    print(folder_name)
+    os.makedirs(folder_name, exist_ok=True)
+    os.chdir(folder_name)
+
+    tables = soup.find_all('table')
+
+    for i, table in enumerate(tables):
+        df = pd.read_html(str(table))[0]
+
+        csv_filename = f'table_{i}.csv'
+        df.to_csv(csv_filename, index=False)
+        print(f"Saved table {i} to {csv_filename} in {folder_name}")
+
+    os.chdir('..')
